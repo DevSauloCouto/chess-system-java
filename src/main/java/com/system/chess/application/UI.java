6cfa004bd61889aec85dc42ae551459ffe07706c
SauloCouto <saulocouto1221@gmail.com>
2024-01-25 18:17:51 -0300

    add method to clear screen

--- src/main/java/com/system/chess/application/UI.java
+++ src/main/java/com/system/chess/application/UI.java
@@ -8,6 +8,11 @@ import java.util.Scanner;
 
 public class UI {
 
+    public static void clearScreen() {
+        System.out.println("\033[H\033[2J");
+        System.out.flush();
+    }
+
     public static ChessPosition readChessPosition(Scanner sc) {
         try {
             String position = sc.nextLine();
