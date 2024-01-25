8b2c5703c060ccdaa7629e79ff0cea34e9496f8c
SauloCouto <saulocouto1221@gmail.com>
2024-01-25 18:18:24 -0300

    calling method clearScreen()

--- src/main/java/com/system/chess/application/Program.java
+++ src/main/java/com/system/chess/application/Program.java
@@ -1,9 +1,11 @@
 package com.system.chess.application;
 
+import com.system.chess.chess.ChessException;
 import com.system.chess.chess.ChessMatch;
 import com.system.chess.chess.ChessPiece;
 import com.system.chess.chess.ChessPosition;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Program {
@@ -14,16 +16,29 @@ public class Program {
         ChessMatch chessMatch = new ChessMatch();
 
         while (true) {
-            UI.printBoard(chessMatch.getPieces());
-            System.out.println("");
-            System.out.print("Source: ");
-            ChessPosition source = UI.readChessPosition(sc);
+            try {
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces());
+                System.out.println("");
+                System.out.print("Source: ");
+                ChessPosition source = UI.readChessPosition(sc);
 
-            System.out.println("");
-            System.out.print("Target: ");
-            ChessPosition target = UI.readChessPosition(sc);
+                System.out.println("");
+                System.out.print("Target: ");
+                ChessPosition target = UI.readChessPosition(sc);
 
-            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+            }
+            catch (ChessException e) {
+                System.out.println(e.getMessage());
+                System.out.println("Press enter to continue");
+                sc.nextLine();
+            }
+            catch (InputMismatchException e) {
+                System.out.println(e.getMessage());
+                System.out.println("Press enter to continue");
+                sc.nextLine();
+            }
         }
 
 
